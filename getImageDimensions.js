8e94a19ad1142aee75fb6ac599e4d7e811bc5789
Galia Ladiray <galia@wlsquare.com>
2013-06-02 10:30:29 +0200

    Iterating over the NodeList the old fashioned way, to avoid weird bugs

--- getImageDimensions.js
+++ getImageDimensions.js
@@ -18,17 +18,22 @@ if(system.args.length > 3){
 
 // Set the page's viewport
 page.viewportSize = {width: viewportWidth, height: viewportHeight};
+page.onError = function (msg, trace) {console.log("ERROR:"+msg);}
 
 // Open the URL
 page.open(url, function(status){
     // Return an array of "imgURL width height" strings
     var imageDimensions = page.evaluate(function(){
-        return [].map.call(document.querySelectorAll('img'), function(elem) {
-            return elem.src + " " + elem.clientWidth + " " + elem.clientHeight;
-        });
+        var images = document.querySelectorAll('img');
+        var ret=[];
+        for(var i = 0; i < images.length; i++){
+            var img = images[i];
+            ret[i] = img.src + " " + img.clientWidth + " " + img.clientHeight;
+        }
+        return ret;
     });
     // Print the dimensions to the console
-    imageDimensions.forEach(function(val){console.log(val)});
+    imageDimensions && imageDimensions.forEach(function(val){console.log(val)});
 
     phantom.exit(0);
 });
