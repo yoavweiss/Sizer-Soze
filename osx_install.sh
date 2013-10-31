b0e5d62ed60c89a9c7d419eb3abdf928c8aba142
Yoav Weiss <yoav@yoav.ws>
2013-10-31 19:07:42 +0100

    add sudo to gem

--- osx_install.sh
+++ osx_install.sh
@@ -3,7 +3,7 @@ ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
 brew update
 brew install python
 brew install phantomjs
-gem install image_optim
+sudo gem install image_optim
 brew install imagemagick
 sudo pip install python-slugify
 brew install libmagic
